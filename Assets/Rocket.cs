f261ca75eb69b633ee816ed11d07823c3e815501
unityudemy <thomaswkhutton@gmail.com>
2019-03-09 21:27:38 -0500

    Rockets and code.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,6 +6,13 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
 
+    Rigidbody rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -16,7 +23,7 @@ public class Rocket : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.Space))
         {
-            print("Space pressed.");
+            rigidBody.AddRelativeForce(Vector3.up);
         }
 
         if (Input.GetKey(KeyCode.A))
