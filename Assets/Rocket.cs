5addc68fcdfd2eb48da2ec0c043acda92d8ecf2a
unityudemy <thomaswkhutton@gmail.com>
2019-03-09 19:54:45 -0500

    Added rocket and Key Inputers

---
+++ Assets/Rocket.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            print("Space pressed.");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating left.");
+        } else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating right.");
+        }
+    }
+}
